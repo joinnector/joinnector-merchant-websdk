61fddd6803e5a3078b54fc4b24bef8cffbf5323e
mcd-50 <ayushshukla@ayushs-MacBook-Pro.local>
2022-06-20 13:17:51 +0530

    Fixes

--- src/component/nector/home_component.js
+++ src/component/nector/home_component.js
@@ -521,7 +521,7 @@ class HomeComponent extends React.Component {
 							</div>
 
 							<div style={{ textAlign: "center", marginTop: 15 }}>
-								<Button type="primary" onClick={() => show_loggedin_referral_card && this.on_referral()}> <span style={{ marginRight: 6 }}>View</span>  <react_material_icons.MdKeyboardBackspace className="nector-backspace-rotate nector-text" style={{ color: websdk_config.text_color }} /> </Button>
+								<Button type="primary" onClick={() => show_loggedin_referral_card && this.on_referral()}> <span style={{ marginRight: 6 }}>Refer &amp; Earn</span>  <react_material_icons.MdKeyboardBackspace className="nector-backspace-rotate nector-text" style={{ color: websdk_config.text_color }} /> </Button>
 							</div>
 						</div>
 					</antd.Card>
